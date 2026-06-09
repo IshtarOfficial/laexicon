d59cd616c509927dd526b8079fbc7195445afed3
ÆVOLUTION <76079289+IshtarOfficial@users.noreply.github.com>
2026-06-09 16:58:01 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/ae-lexicon/', // Required for GitHub Pages
+    base: '/laexicon/', // Required for GitHub Pages
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
